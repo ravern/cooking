8313f320302f9b0ae4b1b4899207d0bbd0ad7409
Ravern Koh <ravern@taskade.com>
2020-05-09 10:36:53 +0800

    chore: configure apollo

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,26 @@
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import { Global, css } from "@emotion/core";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import fetch from "isomorphic-unfetch";
+
+const client = new ApolloClient({
+  uri: '/api',
+  fetch,
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider>
-      <CSSReset />
-      <Global styles={css`
-        body {
-          background-color: #f9f9f9;
-        }
-      `} />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider>
+        <CSSReset />
+        <Global styles={css`
+          body {
+            background-color: #f9f9f9;
+          }
+        `} />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
