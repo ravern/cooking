70fdda4e22385db982f4e7605f37bb57c6bf199d
Ravern Koh <ravernkoh@gmail.com>
2020-08-13 22:32:39 +0800

    experiment with grid

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,7 +1,16 @@
+import styled from "@emotion/styled";
+
 import fetch from "~/helpers/fetch";
 
-export default function IndexPage({ data, error }) {
-  return JSON.stringify(data);
+export default function IndexPage({ dishes, error }) {
+  return (
+    <GridContainer>
+      {dishes.map((dish) => {
+        console.log(dish.images);
+        return <DishContainer key={dish.id} image={dish.images[0]} />;
+      })}
+    </GridContainer>
+  );
 }
 
 export async function getServerSideProps() {
@@ -9,8 +18,26 @@ export async function getServerSideProps() {
 
   return {
     props: {
-      data: data ?? null,
+      dishes: data ?? null,
       error: error ?? null,
     },
   };
 }
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 8px;
+
+  & > *::before {
+    content: "";
+    padding-bottom: 100%;
+    display: inline-block;
+    vertical-align: top;
+  }
+`;
+
+const DishContainer = styled.div`
+  background-image: url(${(props) => props.image});
+  background-size: cover;
+`;
